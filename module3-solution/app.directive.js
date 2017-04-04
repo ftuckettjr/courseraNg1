de26a748392522287277217d639862c54f34abd2
Ftuckettjr <fatjr@fatjr.attlocal.net>
2017-04-03 23:11:29 -0400

    code clean-up2

--- module3-solution/app.directive.js
+++ module3-solution/app.directive.js
@@ -10,19 +10,58 @@
         templateUrl: 'foundItems.html',
         scope: {
           found: '<itemsFound',
-          //searchTerm: '<searchItem',
-          //searchForMatch: '&',
-          //myTitle: '@title',
           removeItem: '&'
-        }//,
-        //controller: ShoppingListDirectiveController,
-        //controllerAs: 'list',
-        //bindToController: true,
-        //link: ShoppingListDirectiveLink,
+        },
+        controller: FoundItemsDirectiveController,
+        controllerAs: 'ctrl',
+        bindToController: true//,
+        //link: FoundItemsDirectiveLink,
         //transclude: true
       };
 
       return ddo;
     }
+
+    function FoundItemsDirectiveController() {
+      var list = this;
+    }
+
+    /*function FoundItemsDirectiveLink(scope, element, attrs, controller) {
+      console.log("Link scope is: ", scope);
+      console.log("Controller instance is: ", controller);
+      console.log("Element is: ", element);
+
+      scope.$watch('list.cookiesInList()', function (newValue, oldValue) {
+        console.log("Old value: ", oldValue);
+        console.log("New value: ", newValue);
+
+        if (newValue === true) {
+          displayCookieWarning();
+        }
+        else {
+          removeCookieWarning();
+        }
+      });
+
+      function displayCookieWarning() {
+        // Using Angular jqLite
+        // var warningElem = element.find("div");
+        // warningElem.css('display', 'block');
+
+        // If jQuery included before Angular
+        var warningElem = element.find("div.error");
+        warningElem.slideDown(900);
+      }
+
+      function removeCookieWarning() {
+        // Using Angular jqLite
+        // var warningElem = element.find('div');
+        // warningElem.css('display', 'none');
+
+        // If jQuery included before Angular
+        var warningElem = element.find('div.error');
+        warningElem.slideUp(900);
+      }
+    }*/
 
 })();
