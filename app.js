5827f9b125faac5c897a2bb23835970a28beb2d6
Ftuckettjr <fatjr@fatjr.attlocal.net>
2017-04-03 23:50:13 -0400

    code clean-up 3

---
+++ app.js
@@ -0,0 +1,12 @@
+
+(function(){
+  'use strict';
+
+  //Single Responsibility
+  angular.module('IndexHomeApp', ['ngRoute']);
+
+  angular.element(document).ready(function() {
+    angular.bootstrap(document, ['IndexHomeApp'], {strictDi: true});
+  });
+
+})();
